beee5b17a01dc3c89f8a6a83c4b901627293d0f5
nanamedou <45792258+nanamedou@users.noreply.github.com>
2018-12-24 12:59:21 +0900

    update
    +遅延とビジュアライザの範囲の入力を設置した。
    !ビジュアライザを虹色にした。

--- plot.js
+++ plot.js
@@ -14,12 +14,33 @@ const max = (array) => {
   return k
 }
 
-
 const min = (array) => {
   let k = array[0]
   for (let i = 1; i < array.length; i++) {
     if (k > array[i]) {
       k = array[i]
+    }
+  }
+  return k
+}
+
+const max2 = (array) => {
+  let k = max(array[0])
+  for (let i = 1; i < array.length; i++) {
+    const s = max(array[i])
+    if (k < array[i]) {
+      k = s
+    }
+  }
+  return k
+}
+
+const min2 = (array) => {
+  let k = min(array[0])
+  for (let i = 1; i < array.length; i++) {
+    const s = min(array[i])
+    if (k < array[i]) {
+      k = s
     }
   }
   return k
@@ -51,17 +72,46 @@ export class Figure {
   constructor(context, rect) {
     this.ctx = context
     this.rect = rect
-    /* グラフ内の範囲の最大最小 */
-    this.minX = null
-    this.maxX = null
-    this.minY = null
-    this.maxY = null
+    /* グラフ内の範囲の最小最大 */
+    this.limX = [null, null]
+    this.limY = [null, null]
+  }
+
+
+  calcRectFrom(ax, ay) {
+    let rangeXY = [this.limX[0], this.limX[1], this.limY[0], this.limY[1]]
+    if (rangeXY[0] == null) {
+      rangeXY[0] = min2(ax)
+    }
+    if (rangeXY[1] == null) {
+      rangeXY[1] = max2(ax)
+    }
+    if (rangeXY[2] == null) {
+      rangeXY[2] = min2(ay)
+    }
+    if (rangeXY[3] == null) {
+      rangeXY[3] = max2(ay)
+    }
+    return [
+      rangeXY[0],
+      rangeXY[3],
+      rangeXY[1] - rangeXY[0],
+      rangeXY[2] - rangeXY[3]]
+  }
+
+
+  clipRect(){
+    let ctx = this.ctx
+    let rect = this.rect
+    ctx.beginPath()
+    ctx.rect(rect[0], rect[1], rect[2], rect[3])
+    ctx.closePath()
+    ctx.clip()
   }
 
 
   /**
    * canvasに線グラフを書く
-   * クラス初期化時に範囲設定してもはみ出す
    * @param {number[]} x - グラフのx値配列
    * @param {number[]} y - グラフのy値配列
    * @public
@@ -71,35 +121,79 @@ export class Figure {
     let rect = this.rect
 
     /* 描画される値の範囲を求める */
-    let rangeXY = [this.minX, this.maxX, this.minY, this.maxY]
-    if (rangeXY[0] == null) {
-      rangeXY[0] = min(x)
-    }
-    if (rangeXY[1] == null) {
-      rangeXY[1] = max(x)
-    }
-    if (rangeXY[2] == null) {
-      rangeXY[2] = min(y)
-    }
-    if (rangeXY[3] == null) {
-      rangeXY[3] = max(y)
-    }
-    const rectFrom = [
-      rangeXY[0],
-      rangeXY[3],
-      rangeXY[1] - rangeXY[0],
-      rangeXY[2] - rangeXY[3]]
+    const rectFrom = this.calcRectFrom([x], [y])
 
     /* x-yの値を線でつないで描画 */
+    ctx.save()
+
+    // 範囲選択
+    this.clipRect()
+
+    ctx.strokeStyle = this.style
+    if(!ctx.strokeStyle){
+      ctx.strokeStyle = 'blue'
+    }
+
     let pos
+    ctx.beginPath()
     pos = transPosInRect(rectFrom, rect, x[0], y[0])
     ctx.moveTo(pos[0], pos[1])
     for (let index = 1; index < x.length; index++) {
       const a = x[index];
       const b = y[index];
       pos = transPosInRect(rectFrom, rect, a, b)
-
       ctx.lineTo(pos[0], pos[1])
     }
+    ctx.stroke()
+
+    ctx.restore()
+  }
+
+
+  /**
+   * canvasの2曲線の間の領域のを塗りつぶす
+   * @param {number[]} x - グラフのx値配列
+   * @param {number[]} y0 - グラフのy値配列
+   * @param {number[]} y1 - グラフのy値配列
+   * @public
+   */
+  fill_between(x, y0, y1) {
+    let ctx = this.ctx
+    let rect = this.rect
+
+    /* 描画される値の範囲を求める */
+    const rectFrom = this.calcRectFrom([x], [y0, y1])
+    
+    /* x-yの値を線でつないで描画 */
+    ctx.save()
+
+    // 範囲選択
+    this.clipRect()
+
+    ctx.fillStyle = this.style
+    if(!ctx.fillStyle){
+      ctx.fillStyle = 'blue'
+    }
+
+    let pos
+    ctx.beginPath()
+    pos = transPosInRect(rectFrom, rect, x[0], y0[0])
+    ctx.moveTo(pos[0], pos[1])
+    for (let index = 1; index < x.length; index++) {
+      const a = x[index]
+      const b = y0[index]
+      pos = transPosInRect(rectFrom, rect, a, b)
+      ctx.lineTo(pos[0], pos[1])
+    }
+    for (let index = x.length - 1; index >= 0; index--) {
+      const a = x[index]
+      const b = y1[index]
+      pos = transPosInRect(rectFrom, rect, a, b)
+      ctx.lineTo(pos[0], pos[1])
+    }
+    ctx.closePath()
+    ctx.fill()
+    
+    ctx.restore()
   }
 }
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 import * as plt from './plot.js'
 
 const FFT_SIZE = 2048
+const DB_LIM = [-150, -30]
 
 /*
  * beginRecord呼び出し時に初期化される変数
  */
-
+let recordingInitialized
 let gainNode
+let delayNode
 let analyserNode
 let freqDatas
+let freqMinDatas
 let hzDatas
 
 /**
@@ -27,103 +30,133 @@ const beginRecord = () => {
 
   if (navigator.mediaDevices.getUserMedia) {
     // メディア使用権限取得
-    navigator.mediaDevices.getUserMedia (
-    {
-      audio: true,
-      video: false
-    }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia(
+      {
+        audio: true,
+        video: false
+      }).then((stream) => {
 
-      /* ノードを作成する */
-      let sourceNode = audioCtx.createMediaStreamSource(stream)
+        /* ノードを作成する */
+        let sourceNode = audioCtx.createMediaStreamSource(stream)
 
-      analyserNode = audioCtx.createAnalyser()
-      analyserNode.fftSize = FFT_SIZE
-      freqDatas = new Float32Array(analyserNode.frequencyBinCount)
-      hzDatas = new Float32Array(analyserNode.frequencyBinCount)
-      for(let i = 0; i < analyserNode.frequencyBinCount; i++){
-        hzDatas[i] = i * audioCtx.sampleRate / analyserNode.fftSize
-      }
+        analyserNode = audioCtx.createAnalyser()
+        analyserNode.fftSize = FFT_SIZE
+        freqDatas = new Float32Array(analyserNode.frequencyBinCount)
+        freqMinDatas = new Array(analyserNode.frequencyBinCount)
+        hzDatas = new Float32Array(analyserNode.frequencyBinCount)
+        for (let i = 0; i < analyserNode.frequencyBinCount; i++) {
+          hzDatas[i] = i * audioCtx.sampleRate / analyserNode.fftSize
+        }
 
-      gainNode = audioCtx.createGain()
-      gainNode.gain.value = 1
-      
-      let dynamicCompressorNode = audioCtx.createDynamicsCompressor()
-      
-      let delayNode = audioCtx.createDelay(1)
-      delayNode.delayTime.value = 0.5
-      
-      /* ノードを接続する */
-      sourceNode.connect(analyserNode)
-      analyserNode.connect(gainNode)
-      gainNode.connect(dynamicCompressorNode)
-      dynamicCompressorNode.connect(delayNode)
-      delayNode.connect(audioCtx.destination)
+        gainNode = audioCtx.createGain()
+        gainNode.gain.value = 1
 
-    }).catch( (err) => {
-      console.log('メディア初期化に失敗しました。: ' + err)
-    })
+        let dynamicCompressorNode = audioCtx.createDynamicsCompressor()
+
+        delayNode = audioCtx.createDelay(3)
+        delayNode.delayTime.value = 0
+
+        /* ノードを接続する */
+        sourceNode.connect(analyserNode)
+        analyserNode.connect(gainNode)
+        gainNode.connect(dynamicCompressorNode)
+        dynamicCompressorNode.connect(delayNode)
+        delayNode.connect(audioCtx.destination)
+
+        recordingInitialized = true
+      }).catch((err) => {
+        console.log('メディア初期化に失敗しました。: ' + err)
+      })
   } else {
     console.log('navigator.mediaDevices.getUserMedia がサポートされていません。')
   }
 }
 
-const main  = () => {
+const main = () => {
   /* 
    * 描画関連設定
    */
 
-   /* Canvas要素の定義など */
-   let cs  = document.getElementById('canvas')
-   let ctx = cs.getContext('2d')
- 
-   /* 描画用データ宣言 */
-   const rect = [0, 0, cs.width, cs.height]
-   let fig = new plt.Figure(ctx, rect)
-   fig.maxY = 10
-   fig.minY = -210
- 
-   /* 描画関数 */
-   let render = ()=>{
-    if(analyserNode){
-      analyserNode.getFloatFrequencyData(freqDatas)
-    }
-    ctx.fillStyle='black'
-    ctx.fillRect(rect[0], rect[1], rect[2], rect[3])
+  /* Canvas要素の定義など */
+  let cs = document.getElementById('canvas')
+  /**
+   * @type {CanvasRenderingContext2D}
+   */
+  let ctx = cs.getContext('2d')
 
-    ctx.strokeStyle='green'
-    ctx.beginPath()
-    if(hzDatas && freqDatas){
-      fig.plot(hzDatas, freqDatas)
+  /* 描画用データ宣言 */
+  const rect = [0, 0, cs.width, cs.height]
+  let fig = new plt.Figure(ctx, rect)
+
+  let figStyle = ctx.createLinearGradient(0, 0, cs.width, 0)
+  figStyle.addColorStop(0, 'red')
+  figStyle.addColorStop(0.25, 'yellow')
+  figStyle.addColorStop(0.5, 'green')
+  figStyle.addColorStop(0.75, 'blue')
+  figStyle.addColorStop(1, 'violet')
+  fig.style = figStyle
+
+  /* 描画関数 */
+  let render = () => {
+    if (recordingInitialized) {
+      analyserNode.getFloatFrequencyData(freqDatas)
+      fig.limY = DB_LIM
+      for (let i = 0; i < analyserNode.frequencyBinCount; i++) {
+        freqMinDatas[i] = DB_LIM[0]
+      }
     }
-    ctx.stroke()
+    ctx.fillStyle = 'black'
+    ctx.fillRect(rect[0], rect[1], rect[2], rect[3])
+    if (recordingInitialized) {
+      fig.fill_between(hzDatas, freqDatas, freqMinDatas)
+    }
 
     setTimeout(render, 10)
   }
- 
-   render()
+
+  render()
 
   /*
    * 音声関連設定
    */
 
   let beginBtn = document.getElementById('startBtn')
-
-  // ボタンが押されたとき録音を開始する。
-  // このときボタンは無効化される。
-  beginBtn.onclick = ()=>{
+  beginBtn.onclick = () => {
+    // ボタンが押されたとき録音を開始する。
+    // このときボタンは無効化される。
     beginBtn.setAttribute('disabled', 'disabled')
     beginRecord()
   }
 
+  /* ボリューム設定 */
   let volumeRng = document.getElementById('volRng')
-
-  // ボリューム設定
-  volumeRng.oninput = ()=>{
-    if(gainNode){
+  volumeRng.oninput = () => {
+    if (recordingInitialized) {
       gainNode.gain.value = volumeRng.value
     }
   }
 
+  /* 遅延設定 */
+  let delayRng = document.getElementById('dlyRng')
+  delayRng.oninput = () => {
+    if (recordingInitialized) {
+      delayNode.delayTime.value = delayRng.value
+    }
+  }
+
+  /* fft範囲設定 */
+  let vvMinRng = document.getElementById('vvminRng')
+  vvMinRng.oninput = () => {
+    if (recordingInitialized) {
+      DB_LIM[0] = vvMinRng.value = Math.min(vvMinRng.value, DB_LIM[1])
+    }
+  }
+  let vvMaxRng = document.getElementById('vvmaxRng')
+  vvMaxRng.oninput = () => {
+    if (recordingInitialized) {
+      DB_LIM[1] = vvMaxRng.value = Math.max(vvMaxRng.value, DB_LIM[0])
+    }
+  }
 }
 
 main()
